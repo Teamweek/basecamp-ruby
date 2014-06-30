bc9e52a1b117536d7172dd54aa092ba3824fdf98
Janika Liiv <janika.liiv@gmail.com>
2014-06-30 17:12:42 +0300

    example added

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ basecamp-ruby
 
 A very lightweight ruby library without an http client for using Basecamp API
 
+### Why clientless?
+
+When integrating with more than one remote API (e.g. Basecamp and Freshbooks) you
+can share one http client class between the multiple API clients, which enables
+unified error and access handling.
+
+### HTTP Client
+
+The HTTP client is responsible for authorization, user agent and HTTP caching headers.
+The HTTP client that is passed to the Basecamp::Client must implement the following methods, that this gem uses:
+  * ```base_uri(path)``` - sets the base uri for the requests
+  * ```get(uri)``` - HTTP GET request, which returns the response body as JSON
---
+++ examples/basic.rb
@@ -0,0 +1,36 @@
+require 'basecamp-ruby'
+require 'httparty'
+
+class ExampleClient
+  USER_AGENT = "basecamp-ruby example (https://github.com/Teamweek/basecamp-ruby)"
+
+  def initialize(username, password)
+    @auth = { username: username, password: password }
+  end
+
+  def get(path)
+    handle_response(HTTParty.get("#{@site}#{path}", options))
+  end
+
+  def options
+    {basic_auth: @auth, headers: {"User-Agent"  => USER_AGENT}}
+  end
+
+  def base_uri(site)
+    @site = site
+  end
+
+  def handle_response(request)
+    if request.response.kind_of? Net::HTTPSuccess
+      request.parsed_response
+    else
+      #place for more sophisticated error handling
+      raise "Something went wrong"
+    end
+  end
+end
+
+http_client = ExampleClient.new('<username>', '<password>')
+basecamp_id = "<basecamp_id>"
+basecamp_client = Basecamp::Client.new(http_client, basecamp_id)
+people = basecamp_client.people
